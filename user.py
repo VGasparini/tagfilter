de015db886476bcc96dce85fd49959b6430b7c7d
Vinicius Gasparini <viniciuszeiko@gmail.com>
2019-12-19 23:50:33 -0200

    correct user_url

--- user.py
+++ user.py
@@ -1,15 +1,17 @@
 class User:
     def __init__(self, author):
-        self.name     = author.name
+        self.name = author.name
         self.username = author.screen_name
-        self.user_url = author.url
+        self.user_url = 'https://twitter.com/'+self.username
 
     def get_name(self):
         return self.name
+
     def get_username(self):
         return self.username
+
     def get_user_url(self):
         return self.user_url
-    
+
     def __str__(self):
         return '@'+self.username
