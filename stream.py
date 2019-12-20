26f0241545d334f68547c394203aba09a4ea8047
Vinicius Gasparini <viniciuszeiko@gmail.com>
2019-12-19 23:49:53 -0200

    rework to store tweets

--- stream.py
+++ stream.py
@@ -1,16 +1,18 @@
 from tweet import *
-from app import *
 
 from tweepy import Stream, StreamListener, API, models
 
 # A listener handles tweets that are received from the stream.
+
+
 class Listener(StreamListener):
     def __init__(self):
         super().__init__()
-        self.counter = 0
-        self.limit = 10
+        self.ready = False
+        self.limit = 3
+        self.tweets = list()
 
-    def set_limit(self,limit):
+    def set_limit(self, limit):
         self.limit = limit
 
     def on_disconnect(self, notice):
@@ -20,13 +22,13 @@ class Listener(StreamListener):
         print("Stream API Connected")
 
     def on_status(self, status):
-        add_tweet(Tweet(status))
+        self.tweets.append((len(self.tweets), Tweet(status)))
 
         # Controlling number o tweets to get
-        if self.counter < self.limit:
-            self.counter += 1
+        if len(self.tweets) < self.limit:
             return True
         else:
+            self.tweets.pop(0)
             return False
 
     def on_error(self, status):
