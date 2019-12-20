c49cac125a9a01a60497ab0cf7c8a550d6d2f715
Vinicius Gasparini <viniciuszeiko@gmail.com>
2019-12-20 15:58:35 -0200

    add method to handle with limited trafic

--- stream.py
+++ stream.py
@@ -1,14 +1,15 @@
 from tweet import *
-
+import time
 from tweepy import Stream, StreamListener, API, models
-
-# A listener handles tweets that are received from the stream.
 
 
 class Listener(StreamListener):
+    '''
+    The Listener class handles tweets that are received from the twitter API-stream.
+    '''
+
     def __init__(self):
         super().__init__()
-        self.ready = False
         self.limit = 3
         self.tweets = list()
 
@@ -21,15 +22,19 @@ class Listener(StreamListener):
     def on_connect(self):
         print("Stream API Connected")
 
+    def on_limit(self, status):
+        print("Rate Limit Exceeded, Sleep for 1 Min")
+        time.sleep(60)
+
     def on_status(self, status):
-        self.tweets.append((len(self.tweets), Tweet(status)))
+        self.tweets.append((status.created_at.time(), Tweet(status)))
 
         # Controlling number o tweets to get
         if len(self.tweets) < self.limit:
             return True
         else:
             self.tweets.pop(0)
-            return False
+            return True
 
     def on_error(self, status):
         print(status)
